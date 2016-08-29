e7b4bee7b873e8e185caaeda908f8a43152fed4f
trikitrok <trikitrok@gmail.com>
2016-08-30 00:05:20 +0200

    encoding numbers greater or equal than 128

--- src/varint/core.clj
+++ src/varint/core.clj
@@ -4,6 +4,17 @@
   (concat (repeat (- 7 (count bin-num)) "0")
           bin-num))
 
+(defn add-msn [bytes]
+  (flatten (concat (map #(cons "1" %) (butlast bytes))
+                   (cons "0" (last bytes)) )))
+
 (defn encode [int-num]
-  (let [bin-num (Long/toBinaryString int-num)]
-    (apply str (cons "0" (pad-to-seven bin-num)))))
+  (->> int-num
+       Long/toBinaryString
+       reverse
+       (partition-all 7 7)
+       (map reverse)
+       (map pad-to-seven)
+       (map #(apply str %))
+       add-msn
+       (apply str)))
--- test/varint/core_test.clj
+++ test/varint/core_test.clj
@@ -9,4 +9,8 @@
     "encoding numbers under 128"
     (encode 1) => "00000001"
     (encode 8) => "00001000"
-    (encode 127) => "01111111"))
+    (encode 127) => "01111111")
+
+  (facts
+    "encoding numbers greater or equal than 128"
+    (encode 300) => "1010110000000010"))
